b600aaa9df799440694e864baff1101bf4430546
Deekshak106 <deekshamogaveera10@gmail.com>
2021-09-22 21:22:36 +0530

    writes arithmatic operator example program

---
+++ ArithmaticOperator.java
@@ -0,0 +1,28 @@
+class ArithmaticOperator{
+public static void main(String[] args){
+int m=60;
+int n=40;
+int addResult = m+n;
+System.out.println("addResult:"+addResult);
+
+int x=70;
+int y=20;
+int subResult = x-y;
+System.out.println("subResult : "+subResult);
+
+int l=6;
+int o=4;
+int mulResult = l*o;
+System.out.println("mulResult : "+mulResult);
+int i=5;
+int j=50;
+int divResult = j/i;
+System.out.println("divResult : "+divResult);
+
+int r=38;
+int k=6;
+int modResult=r/k;
+System.out.println("modResult :"+modResult);
+
+}
+}
